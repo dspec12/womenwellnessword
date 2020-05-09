0c94f9c0b771b6a90aaf6d3ef9e4b28e78d52d5c
dspec12 <danspec12@gmail.com>
2020-05-09 08:42:06 -0400

    cleanup blog models

--- blog/models.py
+++ blog/models.py
@@ -37,16 +37,3 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk, "slug": self.slug})
-
-
-"""
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
-    post = models.ForeignKey(
-        'Post', related_name='comments', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
-"""
