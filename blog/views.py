8333706b7a84f1fd9e63357768844cc1282b3b46
dspec12 <danspec12@gmail.com>
2020-05-13 18:37:10 -0400

    Added published feature

--- blog/views.py
+++ blog/views.py
@@ -21,8 +21,8 @@ def get_category_count():
 
 
 def index(request):
-    featured = Post.objects.filter(featured=True)
-    latest = Post.objects.order_by("-timestamp")[0:3]
+    featured = Post.objects.filter(featured=True, published=True)
+    latest = Post.objects.filter(published=True).order_by("-timestamp")[0:3]
     context = {"featured": featured, "latest": latest}
     return render(request, "index.html", context)
 
